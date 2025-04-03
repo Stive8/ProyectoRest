bac85f156f5e1f1fe4d1f266b18c8379787ef7bd
Brayhan Ortegon <bsortegonbarrios@gmail.com>
2025-04-02 22:14:56 -0500

    Creacion Servidor en Java
    Se usó el framework Springboot para la creación inicial del proyecto del servidor

---
+++ PredioResidencial/src/main/java/com/acueducto/PredioResidencial/PredioResidencialApplication.java
@@ -0,0 +1,13 @@
+package com.acueducto.PredioResidencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredioResidencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredioResidencialApplication.class, args);
+	}
+
+}
---
+++ PredioResidencial/src/test/java/com/acueducto/PredioResidencial/PredioResidencialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acueducto.PredioResidencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredioResidencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
